a72ce685a75e8deea24cf50d2e8ed460de8083c1
HxeiiY <julesndiaye.drive@gmail.com>
2018-08-09 14:45:22 +0200

    Création des Bases : Version 0
    Création de la Page de garde
    Création des pages Admin & User
    Fonctionnalité : VérificationFormulaire

---
+++ CONTROLLER/FormulaireVerif.php
@@ -0,0 +1,43 @@
+<?php
+
+// Prise des elements du formulaire
+$Login = $_POST['PseudoLog'];
+    // var_dump($Login);
+$Mdp = $_POST['MdpLog'];
+    // var_dump($Mdp);
+    
+// Connection à la base de donnée 
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=GameTournement', 'root', '');
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
+
+// Vérifie si le Pseudo & le mot de passe sont dans la base de donnée 
+$requete = $bdd->prepare("SELECT * FROM Utilisateurs WHERE UT_Login =  ?");
+$requete->execute([$Login]);
+//$resulat = $requete->fetchAll();
+
+echo '<pre>';
+$resultat = $requete->fetchAll();
+echo '</pre>';
+
+//Si y'a un retour de la base de donnée
+if (sizeof($resultat) > 0 && $Mdp == $resultat[0]['UT_Mdp']) {
+    if ($resultat[0]['UT_Role'] == 'administrator') {
+            // Si l'utilisateur est administrateur
+        header('Location: ../VIEWS/AdminPage.html');
+    } else {
+            // Si l'utilisateur est un User
+        header('Location:../VIEWS/UserPage.html');
+    }
+} else {
+        // Element incorrect dans le formulaire
+    header('Location: https://Google.fr');
+        // header('Location: ../VIEWS/Homepage.html');
+}
+
+
+
+?> 
